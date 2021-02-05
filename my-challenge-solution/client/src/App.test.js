3c82d347aef8f671fabc90b746d93c7f0ad726db
tom <tomxu@udel.edu>
2021-02-05 08:03:03 -0500

    started test func

--- my-challenge-solution/client/src/App.test.js
+++ my-challenge-solution/client/src/App.test.js
@@ -15,3 +15,13 @@ describe('true is truthy and false is falsy', () => {
     expect(false).toBe(false);
   });
 });
+
+function sum(x, y) {
+  return x + y;
+}
+
+describe('sum', () => {
+  test('sums up two values', () => {
+    expect(sum(2, 4)).toBe(6);
+  });
+});
